a86fd91b677d9a39f12311a5b40163f5ca38e61f
nicolasbes4-cyber <nicolasbes4@gmail.com>
2025-12-18 14:14:56 +0100

    Create initial Visual class for profitability chart

---
+++ frontend/custom-visuals/profitability-chart/visual.ts
@@ -0,0 +1,8 @@
+import powerbi from "powerbi-visuals-api";
+import * as d3 from "d3";
+import * as d3d from "d3-3d";
+
+export class Visual implements powerbi.extensibility.IVisual {
+  constructor(options) { }
+  public update(options) { }
+}
